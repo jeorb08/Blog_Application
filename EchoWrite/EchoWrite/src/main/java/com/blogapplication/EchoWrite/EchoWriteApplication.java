9bb70d309177bdc5797ba65ab3fba8445b3eb0fa
Jannatul Jeor Bushra <jjeorb08@gmail.com>
2024-11-17 14:23:12 +0600

    Add files via upload

---
+++ EchoWrite/EchoWrite/src/main/java/com/blogapplication/EchoWrite/EchoWriteApplication.java
@@ -0,0 +1,13 @@
+package com.blogapplication.EchoWrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoWriteApplication.class, args);
+	}
+
+}
