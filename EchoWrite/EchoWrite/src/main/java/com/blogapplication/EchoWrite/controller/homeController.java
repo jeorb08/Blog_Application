79775f36378c06d0b6b9a0c93b512449165acbc4
Jannatul Jeor Bushra <jjeorb08@gmail.com>
2024-11-19 06:16:13 +0600

    Update application properties

---
+++ EchoWrite/EchoWrite/src/main/java/com/blogapplication/EchoWrite/controller/homeController.java
@@ -0,0 +1,24 @@
+package com.blogapplication.EchoWrite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class homeController {
+        
+        @GetMapping("/")
+        public String index(){
+                return "index";
+        }
+        @GetMapping("/login")
+        public String login(){
+                return "login";
+        }
+
+        @GetMapping("/registration")
+        public String registration(){
+                return "registration";
+        }
+}
